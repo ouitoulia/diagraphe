4169d3eba71e99c167fe217b437abef0e5628602
arturu <arturu@arturu.it>
2023-06-26 06:57:56 +0200

    Fix(scripts): fix get code base

--- scripts/get_ouitoulia_codebase.sh
+++ scripts/get_ouitoulia_codebase.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-composer create ouitoulia/diagraphe:${1} --no-install --no-cache
+# $1 = Ouitoulía version
+# ^10, ^10.1, ecc
+
+#composer create ouitoulia/diagraphe:${1} --no-install --no-cache
+
 composer require drush/drush --no-install
 composer install
